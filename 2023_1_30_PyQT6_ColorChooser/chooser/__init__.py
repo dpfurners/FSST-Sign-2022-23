6bd02568c4bb24a47eb3b7e8311a061840969eae
dpfurners <dpfurner@tsn.at>
2023-02-02 10:35:41 +0100

    Add Window Hight, Width to the chooser module

--- 2023_1_30_PyQT6_ColorChooser/chooser/__init__.py
+++ 2023_1_30_PyQT6_ColorChooser/chooser/__init__.py
@@ -1,7 +1,9 @@
-DIFFICULTIES = {"easy": 9, "medium": 25, "hard": 49}
+DIFFICULTIES = {"easy": 9, "medium": 25, "hard": 49, "insame": 81}
 WINDOW_HIGHT = 400
 WINDOW_WIDTH = 800
 
 __all__ = [
-    "DIFFICULTIES"
+    "DIFFICULTIES",
+    "WINDOW_HIGHT",
+    "WINDOW_WIDTH",
 ]
